81a079d5935c4f01bea73da2a3acaebb283358e5
Robertson, Jess (Mineral Resources, Kensington) <Jesse.Robertson@csiro.au>
2018-05-16 11:43:23 +0800

    Changing API

--- earthchem/__init__.py
+++ earthchem/__init__.py
@@ -1,5 +1,7 @@
 from . import documentation, query
 
+from .query import RESTClientQuery as Query
+
 # Versioneer imports
 from ._version import get_versions
 __version__ = get_versions()['version']
